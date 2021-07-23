3dbb675678fc76d5a1cc60b362cfab4672c55fc9
Satish Patel <tosatishpatel@gmail.com>
2021-07-23 13:58:39 +0530

    Add subtraction with mod funcationality

--- test1.c
+++ test1.c
@@ -3,6 +3,15 @@
 /* function to add variables */
 int fun_add(int a, int b)
 {
-    int ans = a + b;
+    ans = 45; /* default answer*/
     return ans;
+}
+
+int fun_sub(int a, int b)
+{
+    int ans = a > b ? a-b:b-a;
+    if(ans > 100)
+        return ans;
+    else
+        return 250;
 }
